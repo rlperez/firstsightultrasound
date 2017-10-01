ecf5e1d5ac6909a55373d672adbda2457dd06b8e
Rig Perez <rlperez@udel.edu>
2017-10-01 11:11:26 -0400

    Reformat document

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
-    $('a[href*="#"]:not([href="#"])').click(function() {
+    $('a[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -22,12 +22,12 @@
     });
 
     // Closes responsive menu when a link is clicked
-    $('.navbar-collapse>ul>li>a').click(function() {
+    $('.navbar-collapse>ul>li>a').click(function () {
         $('.navbar-collapse').collapse('hide');
     });
 
     // Collapse the navbar when page is scrolled
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
         } else {
@@ -35,6 +35,9 @@
         }
     });
 
+    $('.iw-container').click(function () {
+
+    });
 })(jQuery); // End of use strict
 
 // Google Maps Scripts
@@ -42,7 +45,9 @@ var map = null;
 
 function geocodeAddress(geocoder, resultsMap) {
     var address = document.getElementById('address').value;
-    geocoder.geocode({ 'address': address }, function (results, status) {
+    geocoder.geocode({
+        'address': address
+    }, function (results, status) {
         if (status === 'OK') {
             resultsMap.setCenter(results[0].geometry.location);
             var marker = new google.maps.Marker({
@@ -60,7 +65,7 @@ var centerLong = -76.613576;
 var center = new google.maps.LatLng(centerLat, centerLong);
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
-google.maps.event.addDomListener(window, 'resize', function() {
+google.maps.event.addDomListener(window, 'resize', function () {
     map.setCenter(center);
 });
 
@@ -83,109 +88,91 @@ function init() {
 
         // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
-        styles: [
-            {
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#193341"
-                    }
-                ]
+        styles: [{
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#193341"
+            }]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#2c5a71"
+            }]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#29768a"
             },
             {
-                "featureType": "landscape",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#2c5a71"
-                    }
-                ]
-            },
-            {
-                "featureType": "road",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#29768a"
-                    },
-                    {
-                        "lightness": -37
-                    }
-                ]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#406d80"
-                    }
-                ]
-            },
-            {
-                "featureType": "transit",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#406d80"
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.text.stroke",
-                "stylers": [
-                    {
-                        "visibility": "on"
-                    },
-                    {
-                        "color": "#3e606f"
-                    },
-                    {
-                        "weight": 2
-                    },
-                    {
-                        "gamma": 0.84
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "color": "#ffffff"
-                    }
-                ]
-            },
-            {
-                "featureType": "administrative",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "weight": 0.6
-                    },
-                    {
-                        "color": "#1a3541"
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.icon",
-                "stylers": [
-                    {
-                        "visibility": "on"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#2c5a71"
-                    }
-                ]
+                "lightness": -37
             }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#406d80"
+            }]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#406d80"
+            }]
+        },
+        {
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+                "visibility": "on"
+            },
+            {
+                "color": "#3e606f"
+            },
+            {
+                "weight": 2
+            },
+            {
+                "gamma": 0.84
+            }
+            ]
+        },
+        {
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#ffffff"
+            }]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry",
+            "stylers": [{
+                "weight": 0.6
+            },
+            {
+                "color": "#1a3541"
+            }
+            ]
+        },
+        {
+            "elementType": "labels.icon",
+            "stylers": [{
+                "visibility": "on"
+            }]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#2c5a71"
+            }]
+        }
         ]
     };
 
@@ -209,16 +196,16 @@ function init() {
 
     // InfoWindow content
     var content = '<div id="iw-container">' +
-                      '<div class="iw-title">First Sight Ultrasound</div>' +
-                      '<div class="iw-content">' +
-                        '<p>1134 York Rd. Suite 109' +
-                        '<br>Lutherville, MD 21093' +
-                        '<br>Phone. (555) 555-5555</p>'+
-                      '</div>' +
-                    '</div>';
+        '<div class="iw-title">First Sight Ultrasound</div>' +
+        '<div class="iw-content">' +
+        '<p>1134 York Rd. Suite 109' +
+        '<br>Lutherville, MD 21093' +
+        '<br>Phone. (555) 555-5555</p>' +
+        '</div>' +
+        '</div>';
     var infowindow = new google.maps.InfoWindow({
         content: content,
-        pixelOffset: new google.maps.Size(0,4)
+        pixelOffset: new google.maps.Size(0, 4)
     });
     infowindow.open(map, marker);
 }
