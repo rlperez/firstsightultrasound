61b4a3cdb3fdb372d1ae4064dc110bcb6fa07496
Rig Perez <rlperez@udel.edu>
2017-10-27 23:15:46 -0400

    Make social media links configurable through prismic

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.route('/').get((req, res) => {
                 services: document.data.services
             });
             // console.log(document.data);
+            console.log(document.data.social_media);
         })
         .catch((err) => {
             res.status(500).send(`Error 500: ${err.message}`);
