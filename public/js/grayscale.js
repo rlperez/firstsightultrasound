76867aa9f9070bd90ee6c293362d2715e474a660
Rig Perez <rlperez@udel.edu>
2017-11-03 18:54:02 -0400

    Make phone number correct

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -196,7 +196,7 @@ function init() {
                      '<div class="iw-content">' +
                        '1134 York Rd. Suite 109' +
                          '<br>Lutherville, MD 21093' +
-                         '<br>Phone. (555) 555-5555' +
+                         '<br>Phone. (443) 797-7553' +
                      '</div>' +
                   '</div>';
     var infowindow = new google.maps.InfoWindow({
